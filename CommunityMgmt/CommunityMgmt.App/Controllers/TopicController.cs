06c117f31052d50ea81f6828b94d682e849a6bd8
william delmas <william.delmas@gmail.com>
2015-10-16 16:33:21 +0200

    Add Method in order to getData from blogs

--- CommunityMgmt/CommunityMgmt.App/Controllers/TopicController.cs
+++ CommunityMgmt/CommunityMgmt.App/Controllers/TopicController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace CommunityMgmt.App.Controllers
 {
@@ -12,6 +16,48 @@ namespace CommunityMgmt.App.Controllers
         public PartialViewResult Index()
         {
             return PartialView();
+        }
+
+        public JsonResult GetData()
+        {
+          
+            var rssFeed = new Uri("https://techblog.betclicgroup.com/feed/");
+            var request = (HttpWebRequest)WebRequest.Create(rssFeed);
+            request.Method = "GET";
+            var response = (HttpWebResponse)request.GetResponse();
+            String feedContents;
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+
+                feedContents = reader.ReadToEnd();
+
+            }
+            var document = XDocument.Parse(feedContents);
+            var links = (from p in document.Descendants("item")
+                         select new
+                         {
+                             Title = p.Element("title").Value,
+                             Link = p.Element("link").Value,
+                             Comments = p.Element("comments").Value,
+                             PubDate = DateTime.Parse(p.Element("pubDate").Value),
+                             Description = p.Element("description").Value,
+                             Content = FetchLinksFromSource(p.Elements().Where(r => r.Name.ToString().Contains("content")).FirstOrDefault().Value)
+                         }).ToList();
+            return Json(links, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public List<Uri> FetchLinksFromSource(string htmlSource)
+        {
+            List<Uri> links = new List<Uri>();
+            string regexImgSrc = @"<img[^>]*?src\s*=\s*[""']?([^'"" >]+?)[ '""][^>]*?>";
+            MatchCollection matchesImgSrc = Regex.Matches(htmlSource, regexImgSrc, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            foreach (Match m in matchesImgSrc)
+            {
+                string href = m.Groups[1].Value;
+                links.Add(new Uri(href));
+            }
+            return links;
         }
     }
 }
